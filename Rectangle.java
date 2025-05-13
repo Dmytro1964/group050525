8b0129707c1b598fbd50cf01e9051dab774753df
Dmytro Mietolkin <aldima121964@gmail.com>
2025-05-14 01:36:06 +0200

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,25 @@
+package Lesson05.Homework05;
+
+public class Rectangle {
+
+    double area;
+    double perimeter;
+
+
+    public void double calculateArea (double length, double width){
+        area = length * width;
+
+        public void printResult() {
+            System.out.println(" Area = " + area );
+        }
+        public void double calculatePerimeter(double length, double width){
+            perimeter = 2* (length + width);
+
+            public void printResult1(){System.out.println(" Perimetr = " + perimeter);
+
+        }
+
+
+
+    }
+
---
+++ RectangleDemo.java
@@ -0,0 +1,16 @@
+package Lesson05.Homework05;
+
+public class RectangleDemo {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle();
+
+        rectangle.calculateArea(length: 5 , width: 15);
+
+        rectangle.perimeter(length: 5, width 15);
+
+
+
+
+    }
+}
